6d939d85eb42722e86ff5742a45317ae14dd8e66
Vineet V N <namannavineet@gmail.com>
2024-05-26 14:02:28 +0530

    feat: create rotating sphere with color animation

--- main.js
+++ main.js
@@ -1,24 +1,91 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import "./style.css";
 
-setupCounter(document.querySelector('#counter'))
+// create a scene
+const scene = new THREE.Scene();
+
+// create the mesh
+const geometry = new THREE.SphereGeometry(3, 32, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: "#00ff83",
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Camwera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.5,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+  (sizes.width = window.innerWidth), (sizes.height = window.innerHeight);
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+// adds controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+// create a looped resize
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
+tl.fromTo(".title", { y: "-100%" }, { y: "0%" });
+
+let mouseDown = false;
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    const rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    console.log(newColor);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
